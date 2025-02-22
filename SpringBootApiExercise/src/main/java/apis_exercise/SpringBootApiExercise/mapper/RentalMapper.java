db88d263bc5761f71b9c39d5e72a2d35ea81df77
Gabriel <gabspereira178@gmail.com>
2025-02-22 21:41:35 +0000

    - rental mapper enhance and addition of ignore, target and source for more precise mapping for conversions

--- SpringBootApiExercise/src/main/java/apis_exercise/SpringBootApiExercise/mapper/RentalMapper.java
+++ SpringBootApiExercise/src/main/java/apis_exercise/SpringBootApiExercise/mapper/RentalMapper.java
@@ -10,21 +10,23 @@ import java.util.List;
 
 @Mapper(componentModel = "spring")
 public interface RentalMapper {
-	//to create renting no need for toEntity
 	@Mapping(target = "accountEntity.id", source = "accountId")
-	@Mapping(target = "vehicleEntity.id",source = "vehicleId")
+	@Mapping(target = "vehicleEntity.id", source = "vehicleId")
+	@Mapping(target = "dateReturn", ignore = true)
+	@Mapping(target = "endKilometers", ignore = true)
+	@Mapping(target = "totalPrice", ignore = true)
+	@Mapping(target = "status", ignore = true)
 	RentalEntity toEntityRequest(RentalRequestDto dto);
 
 	@Mapping(target = "accountId", source = "accountEntity.id")
 	@Mapping(target = "vehicleId", source = "vehicleEntity.id")
-	@Mapping(source = "dateStart", target = "dateStart")
-	@Mapping(source = "dateEnd", target = "dateEnd")
-	@Mapping(source = "dateReturn", target = "dateReturn")
-	@Mapping(source = "startKilometers", target = "startKilometers")
-	@Mapping(source = "endKilometers", target = "endKilometers")
-	@Mapping(source = "status", target = "rentalStatus")
+	@Mapping(target = "dateStart", source = "dateStart")
+	@Mapping(target = "dateEnd", source = "dateEnd")
+	@Mapping(target = "dateReturn", source = "dateReturn")
+	@Mapping(target = "startKilometers", source = "startKilometers")
+	@Mapping(target = "endKilometers", source = "endKilometers")
+	@Mapping(target = "rentalStatus", source = "status")
 	RentalResponseDto toDtoResponse(RentalEntity entity);
 
 	List<RentalResponseDto> toDtoList(List<RentalEntity> entities);
-
 }
