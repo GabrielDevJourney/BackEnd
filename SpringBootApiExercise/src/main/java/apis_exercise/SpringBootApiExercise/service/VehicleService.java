fec57eabdcc9ab2fec4159c696d3c542cce990f9
Gabriel <gabspereira178@gmail.com>
2025-02-27 07:52:52 +0000

    - VehicleRepository method to search for all vehicles with status this allows me to see what are does in maintenance for example to update those who can be to available - VehicleService all methods and helpers to enalbe this status feature from "setters" method of status, checker for date of maintenance and update day when can leave, auto update to available based on 2days of maintenance ,

--- SpringBootApiExercise/src/main/java/apis_exercise/SpringBootApiExercise/service/VehicleService.java
+++ SpringBootApiExercise/src/main/java/apis_exercise/SpringBootApiExercise/service/VehicleService.java
@@ -1,12 +1,15 @@
 package apis_exercise.SpringBootApiExercise.service;
 
 import apis_exercise.SpringBootApiExercise.dto.vehicle.VehicleDto;
-import apis_exercise.SpringBootApiExercise.exception.vehicleException.VehicleNotValidYearOfManufacture;
+import apis_exercise.SpringBootApiExercise.entity.VehicleEntity;
+import apis_exercise.SpringBootApiExercise.enums.VehicleStatus;
+import apis_exercise.SpringBootApiExercise.exception.vehicleException.*;
 import apis_exercise.SpringBootApiExercise.mapper.VehicleMapper;
-import apis_exercise.SpringBootApiExercise.repository.AccountRepository;
 import apis_exercise.SpringBootApiExercise.repository.VehicleRepository;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
 import java.time.Year;
 import java.util.List;
 import java.util.Optional;
@@ -50,8 +53,93 @@ public class VehicleService {
 				.orElse(null);
 	}
 
+	public void updateVehicleStatus(Long vehicleId, VehicleStatus newStatus) {
+		VehicleEntity vehicle = vehicleRepository.findById(vehicleId)
+				.orElseThrow(() -> new VehicleNotFoundException(vehicleId));
+
+		switch (newStatus) {
+			case MAINTENANCE:
+				setVehicleStatusToMaintenance(vehicle);
+				break;
+			case DISABLE:
+				setVehicleStatusToDisable(vehicle);
+				break;
+			case AVAILABLE:
+				setVehicleStatusToAvailable(vehicle);
+				break;
+			case RENTED:
+				setVehicleStatusToRented(vehicle);
+				break;
+			default:
+				throw new VehicleInvalidStatusUpdateException(vehicleId,vehicle.getStatus(), newStatus);
+		}
+
+		vehicleRepository.save(vehicle);
+	}
+
 
 	//* HELPERS METHODS
+
+	public void setVehicleStatusToRented(VehicleEntity vehicle){
+		if(vehicle.getStatus() != VehicleStatus.AVAILABLE){
+			throw new VehicleStatusAvailableToRentedException(vehicle.getId());
+		}
+		vehicle.setStatus(VehicleStatus.RENTED);
+	}
+
+	public void setVehicleStatusToMaintenance(VehicleEntity vehicle){
+		if(vehicle.getStatus().equals(VehicleStatus.DISABLE) ){
+			throw new VehicleStatusDisableToMaintenanceException(vehicle.getId());
+		}
+		vehicle.setStatus(VehicleStatus.MAINTENANCE);
+		vehicle.setMaintenanceEndDate(LocalDate.now().plusDays(2));
+	}
+
+	public void setVehicleStatusToDisable(VehicleEntity vehicle){
+		if(vehicle.getStatus().equals(VehicleStatus.RENTED)){
+			throw new VehicleStatusRentedToDisableException(vehicle.getId());
+		}
+		vehicle.setStatus(VehicleStatus.DISABLE);
+	}
+	public void setVehicleStatusToAvailable(VehicleEntity vehicle){
+		if(vehicle.getStatus().equals(VehicleStatus.RENTED)){
+			throw new VehicleInvalidStatusUpdateException(vehicle.getId(),vehicle.getStatus(), VehicleStatus.AVAILABLE);
+		}
+		vehicle.setStatus(VehicleStatus.AVAILABLE);
+	}
+
+	//check every day for vehicles that can be updated to available
+	@Scheduled(fixedRate = 86400000)
+	public void updateMaintenanceVehicles() {
+		List<VehicleEntity> vehiclesInMaintenance = vehicleRepository.findAllByStatus(VehicleStatus.MAINTENANCE);
+		LocalDate today = LocalDate.now();
+
+		for (VehicleEntity vehicle : vehiclesInMaintenance) {
+			if (vehicle.getMaintenanceEndDate().isBefore(today) || vehicle.getMaintenanceEndDate().isEqual(today)) {
+				vehicle.setStatus(VehicleStatus.AVAILABLE);
+				vehicle.setMaintenanceEndDate(null);
+				vehicleRepository.save(vehicle);
+			}
+		}
+	}
+
+	public void checkAndScheduleMaintenanceIfNeeded(VehicleEntity vehicle, int startKilometers, int endKilometers) {
+		int distanceTraveled = endKilometers - startKilometers;
+
+		if (distanceTraveled >= vehicle.getMaintenanceKilometers()) {
+			setVehicleStatusToMaintenance(vehicle);
+		} else {
+			completeRental(vehicle,endKilometers);
+		}
+
+		vehicleRepository.save(vehicle);
+	}
+
+	public void completeRental(VehicleEntity vehicle, int endKilometers) {
+
+		vehicle.setCurrentKilometers(endKilometers);
+		vehicle.setStatus(VehicleStatus.AVAILABLE);
+	}
 
 	private void checkYearOfManufacture(int vehicleYear){
 		int maxYear = Year.now().getValue();
